6175b36339d5beae86a16650523d66edc1563749
jaydenlaturnus <jaydenlaturnus@gmail.com>
2019-12-08 11:57:45 -0700

    Day 3 complete

---
+++ day3/wire_distance.rb
@@ -0,0 +1,59 @@
+#!/usr/bin/ruby
+require 'set'
+
+def get_visited(path)
+    visited = {}
+    x, y = 0, 0
+    steps = 0
+
+    path.split(',') { |code|
+        dir = code[0]
+        amnt = code[1..].to_i
+
+        amnt.times do
+            case dir
+            when 'R'
+                x += 1
+            when 'L'
+               x -= 1
+            when 'U'
+                y += 1
+            when 'D'
+                y -= 1
+            end
+            steps += 1
+            if !visited[[x, y]]
+                visited[[x, y]] = steps
+            end
+        end
+    }
+    return visited
+end
+
+def manhatten_distance()
+    path_1, path_2, = File.read("wire_path.txt").split("\n")
+    intersect = get_visited(path_1).keys & get_visited(path_2).keys
+    
+    distance = intersect.map { |x, y|
+        x.abs + y.abs
+    }.min
+
+    return distance
+end
+
+def fewest_steps()
+    path_1, path_2, = File.read("wire_path.txt").split("\n")
+    visited_1, visited_2 = get_visited(path_1), get_visited(path_2)
+    intersect = visited_1.keys & visited_2.keys
+
+    distance = intersect.map { |x, y|
+        visited_1[[x, y]] + visited_2[[x, y]]
+    }.min
+
+    return distance
+end
+
+distance = manhatten_distance()
+puts "Manhatten Distance: #{distance}"
+distance = fewest_steps()
+puts "Fewest Steps: #{distance}"
